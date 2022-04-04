609c7a4dd06f24232f19ca9c1c65be12442b6e7e
irfan <irfan.shaikh@me.com>
2022-04-04 14:14:43 +0800

    Base commit

--- Learn01.py
+++ Learn01.py
@@ -1,1 +1,14 @@
-print("Here is the PyCharm to Learn.")
+print("Hello World")
+# First Comment
+print("2022a")
+print(-2022)
+print(type(2022))
+print(2+23)
+print(2.5+34.4)
+whtisit=input("Enter your input: ")
+print(type(whtisit))
+if type(whtisit) == str:
+    print("you enter string")
+elif type(whtisit) == int:
+    print("you entered number")
+exit(0)
