a53c9d0c29e17362ce8edddc4d35334eccc4d929
Irfan <sirfan98cs@gmail.com>
2022-04-08 17:52:15 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+system_array = sys.path
+for path_var in system_array:
+    print(path_var)
+
+#datatype
+#Text Type:	str
+x="Python 3.10"
+print(x)
+print(type(x))
+#Numeric Types:	int, float, complex
+#Sequence Types:	list, tuple, range
+#Mapping Type:	dict
+#Set Types:	set, frozenset
+#Boolean Type:	bool
+#Binary Types:	bytes, bytearray, memoryview
+
